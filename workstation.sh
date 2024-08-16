50ffb533b52bb49a7dd4915a95c210f8e71155af
Pablo Ferreiro <me@pabloferreiro.es>
2024-08-16 19:34:33 +0200

    meson & valac

--- workstation.sh
+++ workstation.sh
@@ -96,7 +96,7 @@ ws_pkgs() {
     ws_cp "$SCRIPT_DIR/desktop/VSCodium.desktop" "$DEFAULT_HOME/Desktop"
 
     # -- Programming lang -- #
-    ws_cmd_root apt install -y build-essential php-cli php-xdebug python3 golang
+    ws_cmd_root apt install -y build-essential php-cli php-xdebug python3 golang valac meson
 
     ws_cmd_root curl -fsSL https://deb.nodesource.com/setup_22.x -o /tmp/nodesource_setup.sh
     ws_cmd_root bash /tmp/nodesource_setup.sh
